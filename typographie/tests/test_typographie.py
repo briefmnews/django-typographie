2571a9db2f15c30a97be1663d795c599b310265a
Thomas C <thomas.carbillet@gmail.com>
2019-12-03 16:02:35 +0100

    test(): replace spaces by typographie

--- typographie/tests/test_typographie.py
+++ typographie/tests/test_typographie.py
@@ -1,69 +1,69 @@
-from ..typographie import spaces, typographie
+from ..typographie import typographie
 from ..smartypants import smartyPants
 
 
-class TestTypographieSpaces(object):
+class TestTypographieSpaces:
     def test_div(self):
         text = "<div>un test : test</div>"
-        assert spaces(text) == "<div>un test\xa0: test</div>"
+        assert typographie(text) == "<div>un test\xa0: test</div>"
 
     def test_column(self):
         text = "un test : test"
-        assert spaces(text) == "un test\xa0: test"
+        assert typographie(text) == "un test\xa0: test"
 
     def test_column_with_tags(self):
         text = '<a href="">un test</a> : test'
-        assert spaces(text) == '<a href="">un test</a>\xa0: test'
+        assert typographie(text) == '<a href="">un test</a>\xa0: test'
 
     def test_semi_column(self):
         text = "un test ; test"
-        assert spaces(text) == "un test\xa0; test"
+        assert typographie(text) == "un test\xa0; test"
 
     def test_semi_column_with_tags(self):
         text = '<a href="">un test</a> ; test'
-        assert spaces(text) == '<a href="">un test</a>\xa0; test'
+        assert typographie(text) == '<a href="">un test</a>\xa0; test'
 
     def test_exclamation_mark(self):
         text = "un test ! test"
-        assert spaces(text) == "un test\xa0! test"
+        assert typographie(text) == "un test\xa0! test"
 
     def test_exclamation_mark_with_tags(self):
         text = '<a href="">un test</a> ! test'
-        assert spaces(text) == '<a href="">un test</a>\xa0! test'
+        assert typographie(text) == '<a href="">un test</a>\xa0! test'
 
     def test_interrogation_mark(self):
         text = "un test ! test"
-        assert spaces(text) == "un test\xa0! test"
+        assert typographie(text) == "un test\xa0! test"
 
     def test_interrogation_mark_with_tags(self):
         text = '<a href="">un test</a> ? test'
-        assert spaces(text) == '<a href="">un test</a>\xa0? test'
+        assert typographie(text) == '<a href="">un test</a>\xa0? test'
 
     def test_closing_quotation_mark_with_period(self):
         text = "test\xbb."
-        assert spaces(text) == "test\xa0\xbb."
+        assert typographie(text) == "test\xa0\xbb."
 
     def test_quotation_mark_with_quote(self):
         text = "\xabactualité\xbb</a>."
-        assert spaces(text) == "\xab\xa0actualité\xa0\xbb</a>."
+        assert typographie(text) == "\xab\xa0actualité\xa0\xbb</a>."
 
     def test_with_quote(self):
         text = "<strong>\xabtest\xbb</strong>."
-        assert spaces(text) == "<strong>\xab\xa0test\xa0\xbb</strong>."
+        assert typographie(text) == "<strong>\xab\xa0test\xa0\xbb</strong>."
 
     def test_nonbreakable_after_digit(self):
         text = "10 personnes"
-        assert spaces(text) == "10\xa0personnes"
+        assert typographie(text) == "10\xa0personnes"
 
     def test_nonbreakable_between_digits(self):
         text = "10 000"
-        assert spaces(text) == "10\xa0000"
+        assert typographie(text) == "10\xa0000"
 
     def test_nonbreakable_between_digit_percent(self):
         text = "10%"
-        assert spaces(text) == "10\xa0%"
+        assert typographie(text) == "10\xa0%"
         text = "10 %"
-        assert spaces(text) == "10\xa0%"
+        assert typographie(text) == "10\xa0%"
 
     def test_exponent(self):
         text = "1<sup>er</sup> to see"
