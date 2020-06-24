b4d1a5e672e9bf51d653b6580569f02b2477b6f4
Thomas C <thomas.carbillet@gmail.com>
2020-06-24 17:35:20 +0200

    test: add tests

--- typographie/tests/test_typographie.py
+++ typographie/tests/test_typographie.py
@@ -1,6 +1,7 @@
 import pytest
 
 from ..filters import spaces, smartypants, exponent, indice, metric
+from ..typographie import typographie
 
 
 class TestTypographieSpaces:
@@ -71,6 +72,23 @@ class TestTypographieSpaces:
 
         assert spaces(text) == "To test with an ampersand h&m for example"
 
+    def test_date_comma_percentage(self):
+        text = "En 2018, 51% de la population"
+        assert spaces(text) == "En 2018, 51\xa0% de la population"
+
+    def test_percentage_with_comma(self):
+        text = "On compte 61,4% de la population"
+        assert spaces(text) == "On compte 61,4\xa0% de la population"
+
+
+class TestTypographieCbReContentBetweenTags:
+    @pytest.mark.parametrize(
+        "text, expected",
+        [("En 2018, 51% de la population", "En 2018, 51\xa0% de la population")],
+    )
+    def test_cb_re_content_between_tags(self, text, expected):
+        assert typographie(text) == expected
+
 
 class TestTypographieSmartyPants(object):
     def test_replace_quotes(self):
@@ -83,16 +101,16 @@ class TestTypographieExponent:
         "text, expected",
         [
             ("Le 1er to see", "Le 1<sup>er</sup> to see"),
-            ("Le 3e to see", "Le 3<sup>e</sup> to see"),
+            ("Le 3e to see", "Le 3<sup>e</sup>\xa0to see"),
             ("1er du nom", "1<sup>er</sup> du nom"),
-            ("99e fois", "99<sup>e</sup> fois"),
-            ("999e fois", "999<sup>e</sup> fois"),
+            ("99e fois", "99<sup>e</sup>\xa0fois"),
+            ("999e fois", "999<sup>e</sup>\xa0fois"),
             ("Le Ier to see", "Le I<sup>er</sup> to see"),
             ("Ier arrondissement", "I<sup>er</sup> arrondissement"),
             ("https://www.lemonde.fr/du-Ier", "https://www.lemonde.fr/du-Ier"),
-            ("Le XIXe to see", "Le XIX<sup>e</sup> to see"),
-            ("Le VIe arrondissement", "Le VI<sup>e</sup> arrondissement"),
-            ("Le XXIe siècle", "Le XXI<sup>e</sup> siècle"),
+            ("Le XIXe to see", "Le XIX<sup>e</sup>\xa0to see"),
+            ("Le VIe arrondissement", "Le VI<sup>e</sup>\xa0arrondissement"),
+            ("Le XXIe siècle", "Le XXI<sup>e</sup>\xa0siècle"),
             ("Le XXIe, siècle", "Le XXI<sup>e</sup>, siècle"),
             ("Le XVIIIe, siècle", "Le XVIII<sup>e</sup>, siècle"),
             ("999e, fois", "999<sup>e</sup>, fois"),
@@ -114,6 +132,8 @@ class TestTypographieIndice:
             (" CO2 ", " CO<sub>2</sub> "),
             ("https://www.lemonde.fr/du-CO2", "https://www.lemonde.fr/du-CO2"),
             ("https://www.lemonde.fr/du-CO2 ", "https://www.lemonde.fr/du-CO2 "),
+            ("CO2</strong>", "CO<sub>2</sub></strong>"),
+            ("(CO2)", "(CO<sub>2</sub>)"),
         ],
     )
     def test_indice(self, text, expected):
@@ -125,10 +145,15 @@ class TestTypographieMetric:
         "text, expected",
         [
             (" m2", " m<sup>2</sup>"),
+            (" m2)", " m<sup>2</sup>)"),
             (" km2", " km<sup>2</sup>"),
             (" m3", " m<sup>3</sup>"),
             (" m3,", " m<sup>3</sup>,"),
             (" m22", " m22"),
+            (
+                "Un espace de 34 m2 de surface",
+                "Un espace de 34 m<sup>2</sup>\xa0de surface",
+            ),
         ],
     )
     def test_metric(self, text, expected):
