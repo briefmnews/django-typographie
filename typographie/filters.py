68695b222e8703bd9a4837fc16638e3dfb8e0902
Thomas <thomas.carbillet@gmail.com>
2022-04-07 17:51:35 +0200

    feat: add cybermalveillance.gouv.fr to re_prevent_underline

--- typographie/filters.py
+++ typographie/filters.py
@@ -34,6 +34,7 @@ re_prevent_underline = [
         "Service-public&zwnj;.&zwnj;fr",
     ),
     (re.compile(r"arte\.tv", flags=re.IGNORECASE), "Arte&zwnj;.&zwnj;tv"),
+    (re.compile(r"cybermalveillance\.gouv\.fr", flags=re.IGNORECASE), "Cybermalveillance&zwnj;.&zwnj;gouv&zwnj;.&zwnj;fr"),
 ]
 re_exponent = [
     (re.compile(r"(^|\s)([1I])(er)(\s)"), "\\1\\2<sup>\\3</sup>\xa0"),
